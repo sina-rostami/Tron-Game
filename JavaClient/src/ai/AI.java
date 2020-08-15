32bc1fdf3b14ecfb548a00a9a0bd4ec914fa042b
sinarostami <Sinarostami4188.1@gmail.com>
2020-08-15 21:31:46 +0430

    some ifs

--- JavaClient/src/ai/AI.java
+++ JavaClient/src/ai/AI.java
@@ -11,42 +11,76 @@ import team.koala.chillin.client.RealtimeAI;
 
 public class AI extends RealtimeAI<World, KSObject> {
     private EDirection dir;
-	private int cycleCnt = 0;
+    private int cycleCnt = 0;
 
     public AI(World world) {
         super(world);
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY() - 1).get(world.getAgents().get(this.mySide).getPosition().getX()) == ECell.AreaWall) {
-            dir = EDirection.Down;
-        } else {
-            dir = EDirection.Up;
-        }
+
     }
 
     @Override
     public void initialize() {
+        if (world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY() - 1).get(world.getAgents().get(this.mySide).getPosition().getX()) == ECell.AreaWall) {
+            dir = EDirection.Down;
+        } else {
+            dir = EDirection.Up;
+        }
+        changeDirection(dir);
         System.out.println("initialize");
     }
 
     @Override
     public void decide() {
         cycleCnt++;
+        areaWallCheck();
+        agentWallCheck();
+        System.out.println("decide");
+        changeDirection(dir);
+    }
 
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY() + 1).get(world.getAgents().get(this.mySide).getPosition().getX()) == ECell.AreaWall) {
-    	    dir = EDirection.Right;
+    private void agentWallCheck() {
+        int x = world.getAgents().get(this.mySide).getPosition().getX();
+        int y = world.getAgents().get(this.mySide).getPosition().getY();
+        if (dir == EDirection.Up) {
+            if (world.getBoard().get(y - 1).get(x) == ECell.YellowWall || world.getBoard().get(y - 1).get(x) == ECell.BlueWall)
+                ActivateWallBreaker();
+        } else if(dir == EDirection.Down) {
+            if (world.getBoard().get(y + 1).get(x) == ECell.YellowWall || world.getBoard().get(y + 1).get(x) == ECell.BlueWall)
+                ActivateWallBreaker();
+        } else if(dir == EDirection.Right) {
+            if (world.getBoard().get(y).get(x + 1) == ECell.YellowWall || world.getBoard().get(y).get(x + 1) == ECell.BlueWall)
+                ActivateWallBreaker();
+        } else if(dir == EDirection.Left) {
+            if (world.getBoard().get(y).get(x - 1) == ECell.YellowWall || world.getBoard().get(y).get(x - 1) == ECell.BlueWall)
+                ActivateWallBreaker();
         }
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY()).get(world.getAgents().get(this.mySide).getPosition().getX() + 4) == ECell.YellowWall) {
-            ActivateWallBreaker();
+    }
+
+    private void areaWallCheck() {
+        int x = world.getAgents().get(this.mySide).getPosition().getX();
+        int y = world.getAgents().get(this.mySide).getPosition().getY();
+        if (dir == EDirection.Up) {
+            if (world.getBoard().get(y - 1).get(x) == ECell.AreaWall)
+                dir = (world.getBoard().get(y).get(x + 1) == ECell.AreaWall) ? EDirection.Left : EDirection.Right;
+        } else if(dir == EDirection.Down) {
+            if (world.getBoard().get(y + 1).get(x) == ECell.AreaWall)
+                dir = (world.getBoard().get(y).get(x + 1) == ECell.AreaWall) ? EDirection.Left : EDirection.Right;
+        } else if(dir == EDirection.Right) {
+            if (world.getBoard().get(y).get(x + 1) == ECell.AreaWall)
+                dir = (world.getBoard().get(y + 1).get(x) == ECell.AreaWall) ? EDirection.Up : EDirection.Down;
+        } else if(dir == EDirection.Left) {
+            if (world.getBoard().get(y).get(x - 1) == ECell.AreaWall)
+                dir = (world.getBoard().get(y + 1).get(x) == ECell.AreaWall) ? EDirection.Up : EDirection.Down;
         }
-		System.out.println("decide");
-        world.getScores().get(this.otherSide);
-		changeDirection(dir);
     }
 
 
     public void changeDirection(EDirection direction) {
         final ECommandDirection dir = ECommandDirection.of(direction.getValue());
         this.sendCommand(new ChangeDirection() {
-            { direction = dir; }
+            {
+                direction = dir;
+            }
         });
     }
 
