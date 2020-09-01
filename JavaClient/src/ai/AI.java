054fc246d397fb7a272c09be0b2c07286bb4c954
Arian Bahrampout <dev.arianbahrampour1380@gmail.com>
2020-09-01 22:34:34 +0430

    Final Project

--- JavaClient/src/ai/AI.java
+++ JavaClient/src/ai/AI.java
@@ -6,47 +6,138 @@ import ks.commands.ChangeDirection;
 import ks.commands.ECommandDirection;
 import ks.models.ECell;
 import ks.models.EDirection;
+import ks.models.Position;
 import ks.models.World;
 import team.koala.chillin.client.RealtimeAI;
 
 public class AI extends RealtimeAI<World, KSObject> {
     private EDirection dir;
-	private int cycleCnt = 0;
 
     public AI(World world) {
         super(world);
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY() - 1).get(world.getAgents().get(this.mySide).getPosition().getX()) == ECell.AreaWall) {
-            dir = EDirection.Down;
-        } else {
-            dir = EDirection.Up;
-        }
+
     }
 
     @Override
     public void initialize() {
         System.out.println("initialize");
+        System.out.println(world.getAgents().get(this.mySide).getPosition().getX());
     }
 
     @Override
     public void decide() {
-        cycleCnt++;
+        int currentX = world.getAgents().get(this.mySide).getPosition().getX();
+        int currentY = world.getAgents().get(this.mySide).getPosition().getY();
+        ECell enemyColour = ECell.YellowWall;
+        if (mySide == "Yellow") enemyColour = ECell.BlueWall;
+        boolean choseWisely = false;
 
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY() + 1).get(world.getAgents().get(this.mySide).getPosition().getX()) == ECell.AreaWall) {
-    	    dir = EDirection.Right;
+        //brake nearby enemy walls
+        if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0 || world.getAgents().get(this.mySide).getWallBreakerRemTime() != 0) {
+            if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
+
+            if (world.getBoard().get(currentY + 1).get(currentX) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Down;
+            }
+            if (world.getBoard().get(currentY - 1).get(currentX) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Up;
+            }
+            if (world.getBoard().get(currentY).get(currentX + 1) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Right;
+            }
+            if (world.getBoard().get(currentY).get(currentX - 1) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Left;
+            }
         }
-        if(world.getBoard().get(world.getAgents().get(this.mySide).getPosition().getY()).get(world.getAgents().get(this.mySide).getPosition().getX() + 4) == ECell.YellowWall) {
-            ActivateWallBreaker();
+
+        // Find nearest enemy wall
+        if (choseWisely == false) {
+
+            int nearestX = 1, nearestY = 1, minDistance = 10000000;
+
+            for (int i = 0; i < world.getBoard().size(); i++) {
+                for (int j = 0; j < world.getBoard().get(0).size(); j++) {
+                    int distance = (i - currentY) * (i - currentY) + (j - currentX) * (j - currentX);
+
+                    if (distance < minDistance && world.getBoard().get(i).get(j) == enemyColour) {
+                        nearestY = i;
+                        nearestX = j;
+                        minDistance = distance;
+                    }
+                }
+            }
+            if (nearestY > currentY && world.getBoard().get(currentY + 1).get(currentX) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Down;
+            }
+            if (nearestY < currentY && world.getBoard().get(currentY - 1).get(currentX) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Up;
+            }
+            if (nearestX > currentX && world.getBoard().get(currentY).get(currentX + 1) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Right;
+            }
+            if (nearestX < currentX && world.getBoard().get(currentY).get(currentX - 1) == enemyColour) {
+                choseWisely = true;
+                dir = EDirection.Left;
+            }
+
         }
-		System.out.println("decide");
-        world.getScores().get(this.otherSide);
-		changeDirection(dir);
+
+
+        // Find empty nearby cell
+        if (choseWisely == false) {
+            if (world.getBoard().get(currentY + 1).get(currentX) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Down;
+            }
+            if (world.getBoard().get(currentY - 1).get(currentX) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Up;
+            }
+            if (world.getBoard().get(currentY).get(currentX + 1) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Right;
+            }
+            if (world.getBoard().get(currentY).get(currentX - 1) == ECell.Empty) {
+                choseWisely = true;
+                dir = EDirection.Left;
+            }
+        }
+
+        // Be careful for area walls
+        if (choseWisely == false) {
+            if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0)
+                ActivateWallBreaker();
+            if (world.getBoard().get(currentY + 1).get(currentX) != ECell.AreaWall)
+                dir = EDirection.Down;
+
+            if (world.getBoard().get(currentY - 1).get(currentX) == ECell.AreaWall)
+                dir = EDirection.Up;
+
+            if (world.getBoard().get(currentY).get(currentX + 1) == ECell.AreaWall)
+                dir = EDirection.Right;
+
+            if (world.getBoard().get(currentY).get(currentX - 1) == ECell.AreaWall)
+                dir = EDirection.Left;
+
+        }
+
+        changeDirection(dir);
     }
 
 
     public void changeDirection(EDirection direction) {
         final ECommandDirection dir = ECommandDirection.of(direction.getValue());
         this.sendCommand(new ChangeDirection() {
-            { direction = dir; }
+            {
+                direction = dir;
+            }
         });
     }
 
