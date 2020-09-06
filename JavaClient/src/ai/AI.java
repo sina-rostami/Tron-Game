11eb904af94d0091cce16054853d12d8828569c1
sinarostami <Sinarostami4188.1@gmail.com>
2020-09-06 20:16:43 +0430

    final

--- JavaClient/src/ai/AI.java
+++ JavaClient/src/ai/AI.java
@@ -9,9 +9,9 @@ import ks.models.EDirection;
 import ks.models.World;
 import team.koala.chillin.client.RealtimeAI;
 
-
 public class AI extends RealtimeAI<World, KSObject> {
     private EDirection dir;
+
     private ECell enemyCell;
 
     public AI(World world) {
@@ -26,85 +26,92 @@ public class AI extends RealtimeAI<World, KSObject> {
             dir = EDirection.Up;
         changeDirection(dir);
         enemyCell = mySide.equals("Yellow") ? ECell.BlueWall : ECell.YellowWall;
+        // todo : change enemy colour to enemy cell
         System.out.println("initialize");
     }
 
     @Override
     public void decide() {
+
+
+
         int currentX = world.getAgents().get(this.mySide).getPosition().getX();
         int currentY = world.getAgents().get(this.mySide).getPosition().getY();
-
+        ECell enemyColour = ECell.YellowWall;
+        if (mySide.equals("Yellow")) enemyColour = ECell.BlueWall;
         boolean chooseWisely = false;
 
         //break nearby enemy walls
-        chooseWisely = breakNearbyEnemyWalls(currentX, currentY);
+        chooseWisely = breakNearbyEnemyWalls(currentX, currentY, enemyColour, chooseWisely);
+
 
         // Find nearest enemy wall
-        if (chooseWisely == false)
-            chooseWisely = findNearestEnemyWall(currentX, currentY);
+        if (chooseWisely == false) {
+            chooseWisely = findNearestEnemyWall(currentX, currentY, enemyColour, chooseWisely);
+        }
+
 
         // Find empty nearby cell
-        if (chooseWisely == false)
-            chooseWisely = findNearestEmptyCell(currentX, currentY);
+        if (chooseWisely == false) {
+            chooseWisely = findNearestEmptyCell(currentX, currentY, chooseWisely);
+        }
 
         // Be careful for area walls
-        if (chooseWisely == false)
+        if (chooseWisely == false) {
             beCarefulForAreaWalls(currentX, currentY);
+        }
 
         changeDirection(dir);
-    }
-    private int BFS(int locationX,int locationY){
 
-
-        // todo : delete this after implementing method
-        return 0;
+        if(currentCycle.compareTo(124) > 0)
+        System.out.println(mySide + "  " + chooseWisely + "  " + dir);
     }
 
     private void beCarefulForAreaWalls(int currentX, int currentY) {
         if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0)
             ActivateWallBreaker();
-
+        EDirection temp = null;
         if (world.getBoard().get(currentY + 1).get(currentX) != ECell.AreaWall && dir != EDirection.Up)
-            dir = EDirection.Down;
+            temp = EDirection.Down;
 
         if (world.getBoard().get(currentY - 1).get(currentX) != ECell.AreaWall && dir != EDirection.Down)
-            dir = EDirection.Up;
+            temp = EDirection.Up;
 
         if (world.getBoard().get(currentY).get(currentX + 1) != ECell.AreaWall && dir != EDirection.Left)
-            dir = EDirection.Right;
+            temp = EDirection.Right;
 
         if (world.getBoard().get(currentY).get(currentX - 1) != ECell.AreaWall && dir != EDirection.Right)
-            dir = EDirection.Left;
-
+            temp = EDirection.Left;
+        dir = temp;
     }
 
-    private boolean findNearestEmptyCell(int currentX, int currentY) {
+    private boolean findNearestEmptyCell(int currentX, int currentY, boolean chooseWisely) {
         if (world.getBoard().get(currentY + 1).get(currentX) == ECell.Empty) {
+            chooseWisely = true;
             dir = EDirection.Down;
-            return true;
         }
         if (world.getBoard().get(currentY - 1).get(currentX) == ECell.Empty) {
+            chooseWisely = true;
             dir = EDirection.Up;
-            return true;
         }
         if (world.getBoard().get(currentY).get(currentX + 1) == ECell.Empty) {
+            chooseWisely = true;
             dir = EDirection.Right;
-            return true;
         }
         if (world.getBoard().get(currentY).get(currentX - 1) == ECell.Empty) {
+            chooseWisely = true;
             dir = EDirection.Left;
-            return true;
         }
-        return false;
+        return chooseWisely;
     }
 
-    private boolean findNearestEnemyWall(int currentX, int currentY) {
+    private boolean findNearestEnemyWall(int currentX, int currentY, ECell enemyColour, boolean choseWisely) {
         int nearestX = 1, nearestY = 1, minDistance = 10000000;
 
         for (int i = 0; i < world.getBoard().size(); i++) {
             for (int j = 0; j < world.getBoard().get(0).size(); j++) {
                 int distance = (i - currentY) * (i - currentY) + (j - currentX) * (j - currentX);
-                if (distance < minDistance && world.getBoard().get(i).get(j) == enemyCell) {
+                if (distance < minDistance && world.getBoard().get(i).get(j) == enemyColour) {
                     nearestY = i;
                     nearestX = j;
                     minDistance = distance;
@@ -112,49 +119,49 @@ public class AI extends RealtimeAI<World, KSObject> {
             }
         }
         if (nearestY > currentY && world.getBoard().get(currentY + 1).get(currentX) == ECell.Empty) {
+            choseWisely = true;
             dir = EDirection.Down;
-            return true;
         }
         if (nearestY < currentY && world.getBoard().get(currentY - 1).get(currentX) == ECell.Empty) {
+            choseWisely = true;
             dir = EDirection.Up;
-            return true;
         }
         if (nearestX > currentX && world.getBoard().get(currentY).get(currentX + 1) == ECell.Empty) {
+            choseWisely = true;
             dir = EDirection.Right;
-            return true;
         }
         if (nearestX < currentX && world.getBoard().get(currentY).get(currentX - 1) == ECell.Empty) {
+            choseWisely = true;
             dir = EDirection.Left;
-            return true;
         }
-        return false;
+        return choseWisely;
     }
 
-    private boolean breakNearbyEnemyWalls(int currentX, int currentY) {
+    private boolean breakNearbyEnemyWalls(int currentX, int currentY, ECell enemyColour, boolean choseWisely) {
         if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0 || world.getAgents().get(this.mySide).getWallBreakerRemTime() > 1) {
-            if (world.getBoard().get(currentY + 1).get(currentX) == enemyCell) {
-                if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
+            if (world.getBoard().get(currentY + 1).get(currentX) == enemyColour) {
+                choseWisely = true;
                 dir = EDirection.Down;
-                return true;
-            }
-
-            if (world.getBoard().get(currentY - 1).get(currentX) == enemyCell) {
                 if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
+            }
+            if (world.getBoard().get(currentY - 1).get(currentX) == enemyColour) {
+                choseWisely = true;
                 dir = EDirection.Up;
-                return true;
-            }
-            if (world.getBoard().get(currentY).get(currentX + 1) == enemyCell) {
                 if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
+
+            }
+            if (world.getBoard().get(currentY).get(currentX + 1) == enemyColour) {
+                choseWisely = true;
                 dir = EDirection.Right;
-                return true;
-            }
-            if (world.getBoard().get(currentY).get(currentX - 1) == enemyCell) {
                 if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
+            }
+            if (world.getBoard().get(currentY).get(currentX - 1) == enemyColour) {
+                choseWisely = true;
                 dir = EDirection.Left;
-                return true;
+                if (world.getAgents().get(this.mySide).getWallBreakerCooldown() == 0) ActivateWallBreaker();
             }
         }
-        return false;
+        return choseWisely;
     }
 
 
